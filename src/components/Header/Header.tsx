eac6a3b835b5ec263adec64e74848398784717cd
chv-roman <chv-roman@mail.ru>
2025-09-06 20:05:38 +0300

    - Установлены prettier, clsx, react-router-dom; - Добавлена верстка "Шапки";

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+import classes from "./Header.module.scss"
+import NavigateMenu from "./Navigate/NavigateMenu"
+import { useState } from "react"
+
+export default function Header() {
+  const [isOpen, setOpen] = useState<boolean>(false)
+
+  const openMenuHandler = () => {
+    setOpen(!isOpen)
+  }
+  return (
+    <header className={classes.header}>
+      <button type="button" className={classes.menuToggle} onClick={openMenuHandler}>
+        <span className="hidden">Menu</span>
+        <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="20" height="2.25" fill="black" />
+          <rect y="6.75" width="20" height="2.25" fill="black" />
+          <rect y="13.5" width="20" height="2.25" fill="black" />
+        </svg>
+      </button>
+      <div className={classes.logo}>
+        <Link to={"/"}>BESIDER</Link>
+      </div>
+      <NavigateMenu isOpen={isOpen} openHandler={openMenuHandler} />
+    </header>
+  )
+}
---
+++ src/components/Header/Navigate/NavigateMenu.tsx
@@ -0,0 +1,70 @@
+import clsx from "clsx"
+import classes from "./NavigateMenu.module.scss"
+import { Link } from "react-router-dom"
+
+interface NavigateMenuProps {
+  isOpen: boolean
+  openHandler: () => void
+}
+
+type TNavItem = {
+  title: string
+  link: string
+}
+
+const navItems: TNavItem[] = [
+  {
+    title: "SCIENCE",
+    link: "/"
+  },
+  {
+    title: "GENERAL",
+    link: "/"
+  },
+  {
+    title: "ENTERTAINMENT",
+    link: "/"
+  },
+  {
+    title: "TECHNOLOGY",
+    link: "/"
+  },
+  {
+    title: "BUSINESS",
+    link: "/"
+  },
+  {
+    title: "HEALTH",
+    link: "/"
+  },
+  {
+    title: "SPORTS",
+    link: "/"
+  }
+]
+
+export default function NavigateMenu({ isOpen, openHandler }: NavigateMenuProps) {
+  return (
+    <nav className={clsx(classes.mainNav, isOpen && classes.mainNav_hide)}>
+      <div className={classes.navHeader}>
+        <button type="button" className={classes.menuToggle} onClick={openHandler}>
+          <span className="hidden">Close menu</span>
+          <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 21L1 1M21 1L1 21" stroke="black" stroke-width="2" stroke-linecap="round" />
+          </svg>
+        </button>
+      </div>
+      <div className={classes.navBody}>
+        <ul>
+          {navItems.map((item, ind) => (
+            <li key={`${ind}-${item.title}`}>
+              <Link to={item.link} className={classes.menuItem}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
